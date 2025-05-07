fadea73b3c5e4f2ea7c8d2f3888d65118fab4784
Idowu <okutuboidowu@gmail.com>
2025-05-07 23:31:32 +0100

    update service and controller

--- controller/todos.controller.js
+++ controller/todos.controller.js
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import{
  getTodos,
+ getTodosCompleted,
+ getTodosTitle,
 
 } from "../services/todos.service.js";
 
@@ -10,4 +12,11 @@ route.get("get-todos", async (req,res) => {
     await getTodos(req,res);
 });
 
+route.get("/get-single-user-todos-title", async (req,res) => {
+    await getTodosTitle(req,res);
+});
+
+route.get("/get-single-user-completed", async (req,res) => {
+    await getTodosCompleted(req,res);
+})
 export default route;
--- services/todos.service.js
+++ services/todos.service.js
@@ -1,20 +1,55 @@
-export const getTodos = async (req,res) => {
-    const id = req.params?.id;
-try{
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/todos`
-    );
+export const getTodos = async (req, res) => {
+  const id = req.params?.id;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
     const data = await response.json();
     return res.status(200).json({
-        success: true,
-        message: "todos fetched successfully",
-        comment: data,
+      success: true,
+      message: "todos fetched successfully",
+      comment: data,
     });
-} catch (error) {
+  } catch (error) {
     return res.status(500).json({
-        success: false,
-        message: "failed to fetch todos",
-        error: error.message,
+      success: false,
+      message: "failed to fetch todos",
+      error: error.message,
     });
-}
+  }
+};
+
+export const getTodosTitle = async (req, res) => {
+  const id = req.params?.id;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`
+  );
+  const data = await response.json();
+
+  return res.status(200).json({
+    success: true,
+    message: " todos title successfully fetched",
+    todos: {
+      id: data?.id,
+      todosTitle: data?.title,
+    },
+  });
+};
+
+export const getTodosCompleted = async (req, res) => {
+  const id = req.params?.id;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`
+  );
+
+  const data = await response.json();
+
+  return res.status(200).json({
+    success: true,
+    message: "todos completed successfully fetched",
+    todos: {
+      id: data?.id,
+      todosCompleted: data?.completed,
+    },
+  });
 };
